6498a45bce2f5bfd26320c2875ec3afa081c83c3
Giovanna Oliani <gigiolianijd@gmail.com>
2017-02-16 20:40:47 -0200

    Mail update

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -1,71 +1,81 @@
 <?php
-header('Content-type: application/json');
+//Variáveis
 
-if($_POST)
-{
-    $to_email       = "site@acquaart.com"; //Recipient email, Replace with own email here
+  $nome = $_POST['name'];
+  $email = $_POST['email'];
+  $telefone = $_POST['phone'];
+  $mensagem = $_POST['message'];
+  $data_envio = date('d/m/Y');
+  $hora_envio = date('H:i:s');
+  $arquivo = "
+  <style type='text/css'>
+  body {
+  margin:0px;
+  font-family:Verdane;
+  font-size:12px;
+  color: #666666;
+  }
+  a{
+  color: #666666;
+  text-decoration: none;
+  }
+  a:hover {
+  color: #FF0000;
+  text-decoration: none;
+  }
+  </style>
+    <html>
+        <table width='510' border='1' cellpadding='1' cellspacing='1' bgcolor='#CCCCCC'>
+            <tr>
+              <td>
+  <tr>
+                 <td width='500'>Nome: $nome</td>
+                </tr>
+                <tr>
+                  <td width='320'>E-mail: <b>$email</b></td>
+     </tr>
+      <tr>
+                  <td width='320'>Telefone: <b>$telefone</b></td>
+                </tr>
+                <tr>
+                  <td width='320'>Mensagem: $nome</td>
+                </tr>
+            </td>
+          </tr>
+          <tr>
+            <td>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></td>
+          </tr>
+        </table>
+    </html>
+  ";
 
-    //check if its an ajax request, exit if not
-    if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
+    // emails para quem será enviado o formulário
+   $emailenviar = $email;
+   $destino = "contato@acquaart.com";
+   $assunto = "Contato pelo Site";
 
-        $output = json_encode(array( //create JSON data
-            'type'=>'error',
-            'text' => 'Sorry Request must be Ajax POST'
-        ));
-        die($output); //exit script outputting json data
-    }
+   // É necessário indicar que o formato do e-mail é html
+   $headers  = 'MIME-Version: 1.0' . "\r\n";
+       $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+       $headers .= 'From: $nome <$email>';
+   //$headers .= "Bcc: $EmailPadrao\r\n";
 
-    //Sanitize input data using PHP filter_var().
-    $user_name      = filter_var($_POST["name"], FILTER_SANITIZE_STRING);
-    $user_email     = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
-    $phone_number   = filter_var($_POST["phone"], FILTER_SANITIZE_NUMBER_INT);
-    $message        = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
-
-    //additional php validation
-    if(strlen($user_name)<4){ // If length is less than 4 it will output JSON error.
-        $output = json_encode(array('type'=>'error', 'text' => 'Name is too short or empty!'));
-        die($output);
-    }
-
-    if(!filter_var($user_email, FILTER_VALIDATE_EMAIL)){ //email validation
-        $output = json_encode(array('type'=>'error', 'text' => 'Please enter a valid email!'));
-        die($output);
-    }
+   $enviaremail = mail($destino, $assunto, $arquivo, $headers);
+   if($enviaremail){
+   $mgm = "E-MAIL ENVIADO COM SUCESSO! <br> O link será enviado para o e-mail fornecido no formulário";
+   echo " <meta http-equiv='refresh' content='10;URL=contato.php'>";
+   } else {
+   $mgm = "ERRO AO ENVIAR E-MAIL!";
+   echo "";
+ }
 
 
-    if(!filter_var($phone_number, FILTER_SANITIZE_NUMBER_FLOAT)){ //check for valid numbers in phone number field
-        $output = json_encode(array('type'=>'error', 'text' => 'Enter only digits in phone number'));
-        die($output);
-    }
-
-    if(strlen($message)<3){ //check emtpy message
-        $output = json_encode(array('type'=>'error', 'text' => 'Too short message! Please enter something.'));
-        die($output);
-    }
-
-    //email subject
-    $subject ='New mail via contact form';
-
-    //email body
-    $message_body = $message."\r\n\r\n-".$user_name."\r\n\r\nEmail : ".$user_email."\r\nPhone Number : ". $phone_number ;
-
-    //proceed with PHP email.
-    $headers = 'From: '.$user_name.'<'.$user_email.'>'."\r\n" .
-    'Reply-To: '.$user_name.'<'.$user_email.'>' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-
-    $send_mail = mail($to_email, $subject, $message_body, $headers);
-
-    if(!$send_mail)
-    {
-        //If mail couldn't be sent output error. Check your PHP email configuration (if it ever happens)
-        $output = json_encode(array('type'=>'error', 'text' => 'Could not send mail! Please check your PHP mail configuration.'));
-        die($output);
-    }else{
-        $output = json_encode(array('type'=>'success', 'text' => 'Hi '.$user_name .', thank you for your email, we will get back to you shortly.'));
-        die($output);
-    }
-}
 
 
-?>
+
+
+
+
+
+
+ ?>
